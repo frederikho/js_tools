1fdce7505ce74dbfbfa82742594c778ca71d8673
frederikho <frederik.holtel@gmx.net>
2021-10-25 04:30:07 +0300

    version of flock with ideas from 1000triangle

--- scripts/2/boid.js
+++ scripts/2/boid.js
@@ -4,6 +4,8 @@ class Boid {
 		let randomSeed2 = random(0.0, 1.0);
 		let randomSeed3 = random(0.0, 1.0);
 
+		this.arms = [];
+		this.lastarm = null;
 		this.position = createVector(random(width), random(height));
 		this.velocity = p5.Vector.random2D(); // more like the direction. Sollte ich ändern. #*
 		
@@ -12,44 +14,59 @@ class Boid {
 		this.acceleration = createVector();
 		this.size = 12;
 		this.color = color(random(110, 255),51,51); // Rottöne, not aligned with random
-		this.maxForce = 0.2;
-		this.maxSpeed = 4; 	
+		this.maxForce = 0.4;
+		this.maxSpeed = 3; 	
 		
-		this.initPerceptionRadius = 100;
+		this.initPerceptionRadius = 60;
 		this.perceptionRadius = this.initPerceptionRadius;
 
+		this.shadowDepth = 20; // #* implementieren
 
+		// this.tail = new Tail(
+		// 	this.position.x,
+		// 	this.position.y,
+		// 	Math.floor(random(5, 10)),
+		// 	Math.floor(random(5, 10)),
+		// 	0,
+		// 	verly
+		//   );
+		//   this.tail.setGravity(new Vector(0, 0))
 
 	} 
 
 	edges () {
-		if (this.position.x > width) {
-			this.position.x = 0;
-		} else if (this.position.x < 0) {
-			this.position.x = width
+
+		let perceptionRadius = 25;
+		let desiredForce = createVector();
+		if (this.position.x < perceptionRadius) {
+			desiredForce = new p5.Vector(this.maxSpeed, this.velocity.y);
+		} else if (this.position.x > width - perceptionRadius) {
+			desiredForce = new p5.Vector(-this.maxSpeed, this.velocity.y);
 		}
-		
-		if (this.position.y > height) {
-			this.position.y = 0;
-		} else if (this.position.y < 0) {
-			this.position.y = height
+		if (this.position.y < perceptionRadius) {
+			desiredForce = new p5.Vector(this.velocity.x, this.maxSpeed);
+		} else if (this.position.y > height - perceptionRadius) {
+			desiredForce = new p5.Vector(this.velocity.x, -this.maxSpeed);
 		}
-		
+
+		if (desiredForce.x != 0 || desiredForce.y != 0) {
+			desiredForce.setMag(this.maxSpeed);
+			desiredForce.sub(this.velocity);
+			desiredForce.limit(this.maxForce*1.35); // maxForce. Should be higher than the other forces
+			this.acceleration.add(desiredForce);
+		}	
 	}
 
 	align (flock) {
 		let perceptionRadius = this.perceptionRadius; //distance to other boids taken into account for alignment
+		let perceptionCount = 5;
 		let desiredForce = createVector(); //average
 		let total = 0; 
-		for (let other of flock) {
-			let dis = dist(this.position.x, this.position.y, 
-				other.position.x, other.position.y);
-
-			if (other != this && dis < perceptionRadius){
+		for (const other of quadTree.getItemsInRadius(this.position.x, this.position.y, perceptionRadius, perceptionCount)) {
 				desiredForce.add(other.velocity);	
 				total ++;		
 			}
-		}
+		
 		
 		if (total > 0) {
 			desiredForce.div(total);
@@ -63,18 +80,14 @@ class Boid {
 	}
 
 	cohesion (flock) {
-		let perceptionRadius = this.perceptionRadius; //distance to other boids taken into account for alignment
+		let perceptionRadius = 100; //distance to other boids taken into account for alignment
+		let perceptionCount = 5;
 		let desiredForce = createVector(); //average
 		let total = 0; 
-		for (let other of flock) {
-			let dis = dist(this.position.x, this.position.y, 
-				other.position.x, other.position.y);
-
-			if (other != this && dis < perceptionRadius){
-				desiredForce.add(other.position);	
-				total ++;		
-			}
-		}
+		for (const other of quadTree.getItemsInRadius(this.position.x, this.position.y, perceptionRadius, perceptionCount)) {
+				desiredForce.add(other.position);
+				total ++;
+			}		
 		
 		if (total > 0) {
 			desiredForce.div(total);
@@ -90,23 +103,21 @@ class Boid {
 
 	separation (flock) {
 		let perceptionRadius = this.perceptionRadius; //distance to other boids taken into account for alignment
+		let perceptionCount = 5;
 		let desiredForce = createVector(); //average
 		let total = 0; 
-		for (let other of flock) {
-			let dis = dist(this.position.x, this.position.y, 
-				other.position.x, other.position.y);
-
-			if (other != this && dis < perceptionRadius){
-				let diffToOther = p5.Vector.sub(this.position, other.position);
-				diffToOther.div(dis * dis); // invert the direction
-				desiredForce.add(diffToOther);	
-				total ++;		
-			}
+		for (const other of quadTree.getItemsInRadius(this.position.x, this.position.y, perceptionRadius, perceptionCount)) {
+			const diffToOther = p5.Vector.sub(this.position, other.position);
+			const diffToOtherLength = diffToOther.mag();
+			if (diffToOtherLength === 0) continue;
+			diffToOther.div(diffToOtherLength*diffToOtherLength); // invert the direction
+			desiredForce.add(diffToOther);	
+			total ++;		
 		}
+	
 		
 		if (total > 0) {
 			desiredForce.div(total);
-			
 			desiredForce.setMag(this.maxSpeed);
 			desiredForce.sub(this.velocity);
 			desiredForce.limit(this.maxForce);
@@ -116,14 +127,14 @@ class Boid {
 		
 	}	
 
-	flockBehaviour (flock) { // #* kürzen?
+	flockBehaviour (flock) {
 		
 		let alignment = this.align(flock);
 		let cohesion = this.cohesion(flock);
 		let separation = this.separation(flock);
+		//let avoidEdge = this.edges();
 		
-		
-		alignment.mult(alignSlider.value());
+		alignment.mult(alignmentSlider.value());
 		cohesion.mult(cohesionSlider.value());
 		separation.mult(separationSlider.value());
 		
@@ -132,25 +143,134 @@ class Boid {
 		this.acceleration.add(alignment);
 		this.acceleration.add(cohesion);
 		this.acceleration.add(separation);
+		//this.acceleration.add(avoidEdge);
 		
 
 	}
 
 	update(){
-		this.position.add(this.velocity);
 		this.velocity.add(this.acceleration);
 		this.velocity.limit(this.maxSpeed);
+		this.position.add(this.velocity);
 		this.acceleration.mult(0);
 
-		this.perceptionRadius = this.initPerceptionRadius * perceptionRadiusSlider.value();
+		//this.perceptionRadius = this.initPerceptionRadius * perceptionRadiusSlider.value();
 		
 	}
 
 	show() {
-		strokeWeight(this.size);
-		stroke(this.color);
-		point(this.position.x, this.position.y);
 		
+		let theta = this.velocity.heading() + radians(90);
+		push();
+		translate(this.position.x-this.shadowDepth, this.position.y+this.shadowDepth);
+		rotate(theta);
+		beginShape();
+		noStroke();
+		fill(0,15);
+		vertex(0, -this.r*2);
+		vertex(-this.r, this.r);
+		vertex(this.r, this.r);
+		endShape(CLOSE);
+		pop();
+	
+		push();
+		translate(this.position.x, this.position.y);
+		rotate(theta)
+		beginShape();
+		stroke(255,150);
+		strokeWeight(2);
+		fill(255,50);
+		vertex(0, -this.r * 2);
+		vertex(-this.r, this.r );
+		vertex(this.r, this.r );
+		endShape(CLOSE);
+		pop();
+		this.tailrender();
+	}
+
+	addArm(length){
+		var arm = new Arm(0,0,length,0);
+		if(this.lastarm){
+	
+		  arm.x = this.lastarm.getEndX();
+		  arm.y = this.lastarm.getEndY();
+		  arm.parent = this.lastarm;
+	
+		}else{
+		  arm.x = this.x;
+		  arm.y = this.y;
+		}
+		  this.arms.push(arm);
+		  this.lastarm = arm;
+	}
+	//-------------draw tail;
+	tailrender(){
+
+		for(var i = 0; i < this.arms.length; i++){
+		this.arms[i].render();
+		}
+	}
+
+	drag(x, y){
+
+		this.lastarm.drag(x, y);
 	}
 }
 
+class Arm{
+
+	constructor(x,y,length,angle){
+		this.x = x;
+		this.y = y;
+		this.length = length;
+		this.angle = angle;
+		this.parent = null;
+  
+	  this.shadowDepth = 25;
+  
+  
+	}
+  
+	getEndX(){
+		return this.x + cos(this.angle) * this.length;
+	}
+  
+	getEndY(){
+		return this.y + sin(this.angle) * this.length;
+	}
+  
+	pointAt(x, y) {
+	  let dx = x - this.x;
+	  let dy = y - this.y;
+	  this.angle = atan2(dy, dx);
+	}
+  
+	drag(x, y){
+  
+	   this.pointAt(x, y);
+	   this.x = x - cos(this.angle) * this.length;
+	   this.y = y - sin(this.angle) * this.length;
+	   if (this.parent) {
+  
+		  this.parent.drag(this.x, this.y);
+  
+		}
+  
+	}
+
+	render(){
+		stroke(7,48,42);
+		strokeWeight(1.5);
+		line(this.x, this.y, this.getEndX(), this.getEndY());
+	   //  stroke(7,48,42,25);
+	   //  line(this.x-25, this.y+25, this.getEndX()-25, this.getEndY()+25);
+   
+   
+	 }
+   
+	 update(){
+	   this.getEndX();
+	   this.getEndY();
+	   this.render();
+	 }
+}
--- scripts/2/sketch.js
+++ scripts/2/sketch.js
@@ -1,37 +1,152 @@
 //"use strict" // this code works the modern way. must be in first line
 const flock = [];
 
-let alignSlider, cohesionSlider, separationSlider, perceptionRadiusSlider;
+let quadTree;
+let alignmentSlider, cohesionSlider, separationSlider, perceptionRadiusSlider;
+
+let leafblack;
+let leafblack02;
+let leafblack02shadow;
+let leafwhite01;
+let leafline;
+let leaflineShadow;
+let leafsmall01;
+let leafsmall02;
+let shadowdepth = 80    ;
+let frogleaf;
 
 function setup () {
-    createCanvas(window.innerWidth,window.innerHeight-65);
-    alignSlider = createSlider(0, 5, 1, 0.1);
-    cohesionSlider = createSlider(0, 3  , 1, 0.1);
-    separationSlider = createSlider(0, 3, 1, 0.1);
+
+    canvas = createCanvas(window.innerWidth,window.innerHeight-65);
+
+    alignmentSlider = createSlider(0.5, 2, 1, 0.1);
+    cohesionSlider = createSlider(0, 1, 0.45, 0.1);
+    separationSlider = createSlider(0, 1, 0.59, 0.1);
     perceptionRadiusSlider = createSlider(0.0, 5, 1, 0.1);
+
+    // const verly = new Verly(16, canvas, ctx);
+    quadTree = new QuadTree(Infinity, 30, new Rect(0, 0, width, height));
     for (let i = 0; i < 100; i++){
         flock.push(new Boid());
     }
 }
 
 
- 
 function draw () {
-    background(51);
     
+    quadTree.clear();
+    for (const boid of flock) {
+      quadTree.addItem(boid.position.x, boid.position.y, boid);
+    }
+
+    background(51);
+    quadTree.debugRender();
 
     for (let boid of flock) {
-        boid.edges();
         boid.flockBehaviour(flock);
+        boid.edges();
         boid.update();
         boid.show();    
     }
+
+    //----background
+
+    // big triangle center
+    push();
+    translate(680, -55);
+    beginShape();
+    rotate(-0.4);
+    noStroke();
+    fill(175,51,51,150);
+    vertex(0, 400*1.2);
+    vertex(250*1.2, 0);
+    vertex(500*1.2, 400*1.2);
+    endShape(CLOSE);
+    pop();
+
+    // big triangle center shadow
+    push();
+    translate(680-shadowdepth, -55+shadowdepth);
+    beginShape();
+    rotate(-0.4);
+    noStroke();
+    fill(0,30);
+    vertex(0, 400*1.2);
+    vertex(250*1.2, 0);
+    vertex(500*1.2, 400*1.2);
+    endShape(CLOSE);
+    pop();
+    
+    // big triangle top left
+    push();
+    translate(-140, -10);
+    beginShape();
+    rotate(-0.8);
+    noStroke();
+    fill(175,51,51,150);
+    vertex(0, 400*0.45);
+    vertex(250*0.45, 0);
+    vertex(500*0.45, 400*0.45);
+    endShape(CLOSE);
+    pop();
+
+    // where?
+    push();
+    translate(580, 680);
+    beginShape();
+    rotate(-0.5);
+    noStroke();
+    fill(175,51,51,150);
+    vertex(0, 400*0.06);
+    vertex(250*0.06, 0);
+    vertex(500*0.06, 400*0.06);
+    endShape(CLOSE);
+    pop();
+
+
+
+    // push();
+    // translate(225, 110);
+    // beginShape();
+    // rotate(0.3);
+    // image(leafwhite01, 0,0);
+    // pop();
+
+
+
+    function drawLeaf(x,y,s){
+        push();
+        translate(width*x, height*y);
+        beginShape();
+        rotate(0.3);
+        noStroke();
+        fill(0,184,147,180);
+        vertex(0, 400*1.4*s);
+        vertex(250*1.4*s, 0);
+        vertex(500*1.4*s, 400*1.4*s);
+        endShape(CLOSE);
+        pop();
+
+    }
+
+    noStroke();
+    fill(255);
+    textSize(12);
+    text("alignment", alignmentSlider.x  -75 , windowHeight - 115);
+    text("separaration", alignmentSlider.x  -75 , windowHeight -85);
+    text("cohesion", alignmentSlider.x  -75, windowHeight-55);
+    text("Craig Reynold's Boids, Daniel shiffman's nature of code", alignmentSlider.x  -103, windowHeight-140);
+    textFont("Helvetica");
+    textSize(54);
+    text("1000 Triangles", alignmentSlider.x  -160 , windowHeight - 165);
+
 
 }
 
 function getRandomVal (randomSeed, min, max) {
     return min + (randomSeed * (max-min));
 }
+
 
 // to do:
 // spatial division optimisation: quadtree, subdivision
@@ -42,5 +157,5 @@ function getRandomVal (randomSeed, min, max) {
 // angualar view of boid. boid wants to keep view empty.
 // submit link to coding challenge
 // traces, view radius option
-// mouse interaction, vgl http://1000triangle.com/
-
+// mouse interaction, vgl http://1000triangle.com/ https://anuraghazra.dev/parasites/
+// in normale Website integrieren
